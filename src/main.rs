3cdf1b1bb66987f6e89e054c152b3ceb4dcf8366
CongLuanTran <congluan.tran19@gmail.com>
2025-05-09 01:34:05 +0700

    feat: implement type

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process;
 fn main() {
     // Declare a variable to hold user input
     let mut input = String::new();
+    let builtin = ["exit", "echo", "type"];
 
     loop {
         // Print the prompt
@@ -20,6 +21,13 @@ fn main() {
         match args[0] {
             "exit" => process::exit(args[1].parse::<i32>().unwrap()),
             "echo" => println!("{}", input.trim().strip_prefix("echo ").unwrap()),
+            "type" => {
+                if builtin.contains(&args[1]) {
+                    println!("{} is a shell builtin", args[1]);
+                } else {
+                    println!("{}: not found", args[1]);
+                }
+            }
             _ => println!("{}: command not found", input.trim()),
         }
     }
