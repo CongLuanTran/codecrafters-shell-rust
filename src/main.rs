af55327a99e0ea879cf267cd3ece91b2ae730a7b
Cong Luan Tran <congluan.tran19@gmail.com>
2025-05-18 10:10:44 +0700

    refactor: turn Shell into its own struct

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,87 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{env, process};
+use std::{
+    env::{self},
+    path::PathBuf,
+    process,
+};
 
 use is_executable::IsExecutable;
 
-fn echo(input: &str) {
-    println!("{}", input.trim());
+struct Shell {
+    input: String,
+    builtin: Vec<&'static str>,
+    path_dirs: Vec<PathBuf>,
 }
 
-fn exit(code: i32) {
-    process::exit(code);
-}
-
-fn type_of(cmd: &str) {
-    let builtin = ["echo", "type", "exit"];
-    if builtin.contains(&cmd) {
-        println!("{} is a shell builtin", cmd);
-    } else {
+impl Shell {
+    fn new() -> Self {
+        let input = String::new();
+        let builtin = vec!["echo", "type", "exit"];
         let path_var = env::var("PATH").unwrap_or_default();
-        let path_dirs = env::split_paths(&path_var);
-        for dir in path_dirs {
+        let path_dirs = env::split_paths(&path_var).collect();
+        Shell {
+            input,
+            builtin,
+            path_dirs,
+        }
+    }
+
+    fn run(&mut self) {
+        loop {
+            // Print the prompt
+            print!("$ ");
+            io::stdout().flush().unwrap();
+
+            // Empty the input
+            self.input.clear();
+            // Wait for user input
+            io::stdin().read_line(&mut self.input).unwrap();
+            let parts: Vec<&str> = self.input.split_whitespace().collect();
+            match parts.as_slice() {
+                [] => {}
+                [cmd] => println!("{}: command not found", cmd),
+                [cmd, args @ ..] => match *cmd {
+                    "echo" => Shell::echo(&args.join(" ")),
+                    "exit" => Shell::exit(args[0].parse::<i32>().unwrap()),
+                    "type" => self.type_of(args[0]),
+                    _ => println!("{}: command not found", cmd),
+                },
+            };
+        }
+    }
+
+    fn find_executable(&self, cmd: &str) -> Option<PathBuf> {
+        for dir in &self.path_dirs {
             let full_path = dir.join(cmd);
             if full_path.is_file() && full_path.is_executable() {
-                println!("{} is {}", cmd, full_path.display());
-                return;
+                return Some(full_path);
             }
         }
-        println!("{}: not found", cmd);
+        None
+    }
+
+    fn echo(input: &str) {
+        println!("{}", input.trim());
+    }
+
+    fn exit(code: i32) {
+        process::exit(code);
+    }
+
+    fn type_of(&self, cmd: &str) {
+        if self.builtin.contains(&cmd) {
+            println!("{} is a shell builtin", cmd);
+        } else {
+            match self.find_executable(cmd) {
+                Some(path) => println!("{} is {}", cmd, path.display()),
+                None => println!("{}: not found", cmd),
+            }
+        }
     }
 }
 
 fn main() {
-    // Declare a variable to hold user input
-    let mut input = String::new();
-
-    loop {
-        // Print the prompt
-        print!("$ ");
-        io::stdout().flush().unwrap();
-
-        // Empty the input
-        input.clear();
-
-        // Wait for user input
-        io::stdin().read_line(&mut input).unwrap();
-        let parts: Vec<&str> = input.split_whitespace().collect();
-        match parts.as_slice() {
-            [] => {}
-            [cmd] => println!("{}: command not found", cmd),
-            [cmd, args @ ..] => match *cmd {
-                "echo" => echo(&args.join(" ")),
-                "exit" => exit(args[0].parse::<i32>().unwrap()),
-                "type" => type_of(args[0]),
-                _ => println!("{}: command not found", cmd),
-            },
-        };
-    }
+    let mut shell = Shell::new();
+    shell.run();
 }
